9df5e68339f6dcd6d79772f71b5bb5420a80106b
bananenbiegerei <74664048+bananenbiegerei@users.noreply.github.com>
2026-01-20 13:17:10 +0100

    style tile nicer colors

--- template-parts/style-tile/colors.php
+++ template-parts/style-tile/colors.php
@@ -1,14 +1,22 @@
-<div class="grid grid-cols-4 gap-8 my-12" x-data="{ colors: ['white', 'black', 'primary', 'secondary', 'accent', 'neutral', 'error', 'success', 'warning', 'muted', 'destructive'] }">
+<div class="grid grid-cols-4 gap-8 my-12"
+    x-data="{ colors: ['white', 'black', 'primary', 'secondary', 'accent', 'neutral', 'error', 'success', 'warning', 'muted', 'destructive'] }">
     <template x-for="(color, index) in colors" :key="index">
-        <div :class="`bg-${color} p-4`">
-            <p x-text="color"></p>
-            <div x-data="{ shades: ['dark', 'light'] }">
-                <div class="grid grid-cols-2"> <!-- Adjusted grid-cols based on the number of items -->
-                    <template x-for="shade in shades">
-                        <div class="p-2 text-center" :class="`bg-${color}-${shade}`" x-text="shade"></div>
-                    </template>
+        <div class="border rounded-lg overflow-hidden">
+            <header class="p-4">
+                <h2 x-text="color"></h2>
+            </header>
+
+            <section :class="`bg-${color} p-4`">
+                <div x-data="{ shades: ['dark', 'light'] }">
+                    <div class="flex">
+                        <!-- Adjusted grid-cols based on the number of items -->
+                        <template x-for="shade in shades">
+                            <div class="p-2 text-center rounded-full h-8 w-8" :class="`bg-${color}-${shade}`"></div>
+                        </template>
+                    </div>
                 </div>
-            </div>
+            </section>
         </div>
+
     </template>
 </div>
