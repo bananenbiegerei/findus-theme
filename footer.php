7ac9305d8a75c750b04233ceefa00fbc22e1a946
bananenbiegerei <74664048+bananenbiegerei@users.noreply.github.com>
2026-01-21 18:33:25 +0100

    latest with flexible content setup

--- footer.php
+++ footer.php
@@ -1,6 +1,5 @@
 </main>
 <?php get_template_part('bb-components/bb-footers/bb-footer-simple/render') ?>
-
 <?php wp_footer(); ?>
 </body>
 
--- front-page.php
+++ front-page.php
@@ -1,9 +1,3 @@
 <?php get_header(); ?>
-<div class="container">
-    <?php get_template_part("template-parts/style-tile/typography"); ?>
-    <?php get_template_part("template-parts/style-tile/typography"); ?>
-    <?php get_template_part("template-parts/style-tile/buttons"); ?>
-
-    <?php get_template_part("template-parts/style-tile/colors"); ?>
-</div>
+<?php get_template_part('bb-blocks/bb-block-loop'); ?>
 <?php get_footer(); ?>
