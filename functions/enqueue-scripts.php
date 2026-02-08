d2643f62903ad24507dd274a5d870e02d8c8cf45
bananenbiegerei <74664048+bananenbiegerei@users.noreply.github.com>
2026-02-08 16:33:23 +0100

    dequed wp frontend css

--- functions/enqueue-scripts.php
+++ functions/enqueue-scripts.php
@@ -19,6 +19,13 @@ add_action('enqueue_block_editor_assets', function () {
     }
 });
 
+// Remove default WordPress block library styles (we use custom Tailwind styles)
+add_action('wp_enqueue_scripts', function () {
+    wp_dequeue_style('wp-block-library');
+    wp_dequeue_style('wp-block-library-theme');
+    wp_dequeue_style('global-styles');
+}, 100);
+
 // Site scripts and style
 add_action('wp_enqueue_scripts', function () {
     $js_file = get_template_directory() . '/js/site.js';
