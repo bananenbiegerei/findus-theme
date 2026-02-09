7174cda1de33059e86879e03e834878445ccdac0
bananenbiegerei <74664048+bananenbiegerei@users.noreply.github.com>
2026-02-09 11:28:19 +0100

    starting with referenzen

--- functions/enqueue-scripts.php
+++ functions/enqueue-scripts.php
@@ -19,12 +19,18 @@ add_action('enqueue_block_editor_assets', function () {
     }
 });
 
-// Remove default WordPress block library styles (we use custom Tailwind styles)
-add_action('wp_enqueue_scripts', function () {
-    wp_dequeue_style('wp-block-library');
-    wp_dequeue_style('wp-block-library-theme');
-    wp_dequeue_style('global-styles');
-}, 100);
+// DISABLE GUTENBERG STYLE | WordPress 5.9
+remove_action( 'wp_enqueue_scripts', 'wp_enqueue_global_styles' );
+remove_action( 'wp_footer', 'wp_enqueue_global_styles', 1 );
+remove_action( 'wp_body_open', 'wp_global_styles_render_svg_filters' );
+
+add_action( 'wp_enqueue_scripts', function() {
+	wp_dequeue_style( 'wp-block-library' );
+	wp_dequeue_style( 'wp-block-library-theme' );
+	wp_dequeue_style( 'classic-theme-styles' );
+});
+
+add_filter( 'should_load_separate_core_block_assets', '__return_true' );
 
 // Site scripts and style
 add_action('wp_enqueue_scripts', function () {
