827c1b933baec20a7f48095e62e0e1091796e307
bananenbiegerei <74664048+bananenbiegerei@users.noreply.github.com>
2026-02-18 16:50:46 +0100

    bkp

--- page.php
+++ page.php
@@ -3,7 +3,7 @@ $color_scheme = get_field('color_scheme');
 ?>
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 	<div class="container">
-		<div class="p-4 rounded-lg md:rounded-xl bg-<?= $color_scheme; ?> min-h-[400px] flex flex-col mb-2">
+		<div class="p-4 rounded-lg md:rounded-xl bg-<?= $color_scheme; ?>-400 min-h-[400px] flex flex-col mb-2 text-black">
 			<div class="flex-1">
 				<h1 class="text-xl md:text-3xl"><?php the_title(); ?></h1>
 			</div>
